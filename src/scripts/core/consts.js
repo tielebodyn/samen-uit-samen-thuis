2318fe94132911d12bb529924244e4949a148cc9
tiele bodyn <tielebodyn@MacBook-Pro-van-tiele.local>
2022-08-10 09:43:05 +0200

    changed to navigo version 7.0.0

--- src/scripts/core/consts.js
+++ src/scripts/core/consts.js
@@ -1,7 +1,7 @@
-const basePath = 'https://main--resplendent-meerkat-743cd1.netlify.app'
+const basePath = 'http://localhost:1234'
+// 'https://main--resplendent-meerkat-743cd1.netlify.app'
 const SENDGRID_API_KEY = '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'
 const MAPBOX_TOKEN = 'pk.eyJ1IjoidGllbGVib2R5biIsImEiOiJjbDZrcXE1cXMwNGV5M2ptbzdsNG04OHFuIn0.mRHi10TXUamkZMoUUmztGg'
-const ENV = 'prod'
 const MELDET_EMAIL = 'tielbody@student.arteveldehs.be'
 const routes = {
     index: basePath,
@@ -16,6 +16,6 @@ const routes = {
     meldet:`${basePath}/meldet`
 }
 export {
-    ENV, routes, MELDET_EMAIL, SENDGRID_API_KEY, MAPBOX_TOKEN
+    routes, MELDET_EMAIL, SENDGRID_API_KEY, MAPBOX_TOKEN
     
 }
