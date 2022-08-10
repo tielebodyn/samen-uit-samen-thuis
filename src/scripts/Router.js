238aecf67bb5fdc79ddbca9882dbd477ad7212ef
tiele bodyn <tielebodyn@MacBook-Pro-van-tiele.local>
2022-08-10 09:33:42 +0200

    tried protocol/host for rootURL

--- src/scripts/Router.js
+++ src/scripts/Router.js
@@ -8,8 +8,7 @@ const Router = {
     router: null,
     getRouter() {
         if (!this.router) {
-            const rootUrl = 'https://main--resplendent-meerkat-743cd1.netlify.app'
-            // `${window.location.protocol}//${window.location.host}`;
+            const rootUrl = `${window.location.protocol}//${window.location.host}`;
             this.router = new Navigo(rootUrl, false)
             }
             return this.router;
