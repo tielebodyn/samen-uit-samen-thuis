9387b7d37d11f5de78439c9e6ec3d1a0be4d12fb
tiele bodyn <tielebodyn@MacBook-Pro-van-tiele.local>
2022-08-10 11:38:22 +0200

    fixed build url

--- src/scripts/core/consts.js
+++ src/scripts/core/consts.js
@@ -1,5 +1,4 @@
-const basePath = 'http://localhost:1234'
-//'https://main--resplendent-meerkat-743cd1.netlify.app'
+const basePath = 'https://main--resplendent-meerkat-743cd1.netlify.app'
 const SENDGRID_API_KEY = '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'
 const MAPBOX_TOKEN = 'pk.eyJ1IjoidGllbGVib2R5biIsImEiOiJjbDZrcXE1cXMwNGV5M2ptbzdsNG04OHFuIn0.mRHi10TXUamkZMoUUmztGg'
 const MELDET_EMAIL = 'tielbody@student.arteveldehs.be'
